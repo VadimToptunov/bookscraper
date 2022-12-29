97bb15f240e687b7cd5cc16b1196758c965c34e5
vadimtoptunov <vtoptunov88@gmail.com>
2022-12-29 22:13:30 +0200

    Add tries to catch errors

--- fantasy_scraper.py
+++ fantasy_scraper.py
@@ -1,10 +1,14 @@
 import os
+import random
+import time
 
 import requests
 import pypandoc
 from bs4 import BeautifulSoup
+from requests import ReadTimeout
 
-categories = [2, 3, 4, 5, 6, 8, 43, 54, 74, 75, 91]
+# categories = [2, 3, 4, 5, 6, 8, 43, 54, 74, 75, 91]
+categories = [3, 4, 5, 6, 8, 43, 54, 74, 75, 91]
 basic_url = "http://book-online.com.ua/index.php?cat="
 blocker = "Доступ к книге заблокирован по требованию правообладателя!"
 
@@ -12,15 +16,25 @@ blocker = "Доступ к книге заблокирован по требов
 def request_categories():
     for i in categories:
         url = f"{basic_url}{i}"
-        bs = parse_page(url)
+        try:
+            bs = parse_page(url)
+        except ReadTimeout:
+            __sleep()
+            print("Have to sleep")
+            bs = parse_page(url)
         last_link = get_last_page(bs)
         paginate(last_link, url)
 
 
 def paginate(last_link, url):
     last_link_range = int(last_link) + 1
+    time.sleep(random.randint(1, 120))
     for i in range(1, last_link_range):
-        soup = parse_page(f"{url}&page={i}")
+        try:
+            soup = parse_page(f"{url}&page={i}")
+        except ReadTimeout:
+            __sleep()
+            soup = parse_page(f"{url}&page={i}")
         blocks = soup.find_all("div", {"class": "block1"})
         for block in blocks:
             book_data = block.find_all("a")
@@ -35,8 +49,7 @@ def save_to_file(filename, text):
 
 def parse_page(url):
     resp = requests.get(url)
-    bs = BeautifulSoup(resp.text, "html.parser")
-    return bs
+    return BeautifulSoup(resp.text, "html.parser")
 
 
 def get_last_page(soup_object):
@@ -45,8 +58,12 @@ def get_last_page(soup_object):
 
 
 def get_book_text(url):
-    soup = parse_page(url)
-    return soup.find("div", {"id": "ptext"}).text
+    try:
+        soup = parse_page(url)
+        return soup.find("div", {"id": "ptext"}).text
+    except ReadTimeout:
+        __sleep()
+        get_book_text(url)
 
 
 def get_books(book_data):
@@ -57,7 +74,10 @@ def get_books(book_data):
     category = book_data[2].text
     filename = f"{category}__{author}-{book_name}"
     if blocker not in book_text:
-        soup = parse_page(book)
+        try:
+            soup = parse_page(book)
+        except ReadTimeout:
+            soup = parse_page(book)
         last_book_page = int(get_last_page(soup)) + 1
         for i in range(1, last_book_page):
             book_text = get_book_text(f"{book}&page={i}")
@@ -69,6 +89,11 @@ def get_books(book_data):
     else:
         print(f"{filename} - Ignored")
         pass
+
+
+def __sleep():
+    time.sleep(30)
+    print("Have to sleep")
 
 
 if __name__ == '__main__':
